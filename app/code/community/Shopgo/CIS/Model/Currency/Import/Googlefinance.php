2dc1a47d55255717b2a7daf7e6f3d57a2cc9fb79
am-shopgo <ammar@shopgo.me>
2014-08-12 05:24:28 -0400

    First commit.

---
+++ app/code/community/Shopgo/CIS/Model/Currency/Import/Googlefinance.php
@@ -0,0 +1,55 @@
+<?php
+
+class Shopgo_CIS_Model_Currency_Import_Googlefinance extends Mage_Directory_Model_Currency_Import_Abstract
+{
+    protected $_url = 'http://www.google.com/finance/converter?a=1&from={{CURRENCY_FROM}}&to={{CURRENCY_TO}}';
+    protected $_messages = array();
+
+    /**
+     * HTTP client
+     *
+     * @var Varien_Http_Client
+     */
+    protected $_httpClient;
+
+    public function __construct()
+    {
+        $this->_httpClient = new Varien_Http_Client();
+    }
+
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, $this->_url);
+        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+
+        try {
+            sleep(Mage::getStoreConfig('currency/google_finance/delay')); // Be nice to Google, they don't have a lot of hi-spec servers!
+
+            $response = $this->_httpClient
+                ->setUri($url)
+                ->setConfig(array('timeout' => Mage::getStoreConfig('currency/google_finance/timeout')))
+                ->request('GET')
+                ->getBody();
+
+            $data = explode('bld>', $response);
+            $data = explode($currencyTo, $data[1]);
+
+            $exchangeRate = 0;
+
+            if(empty($data[0])) {
+                $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s', $url);
+                return null;
+            } else {
+                $exchangeRate = $data[0];
+            }
+
+            return (float) $exchangeRate * 1.0; // Change 1.0 to influence rate
+        } catch (Exception $e) {
+            if($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s', $url);
+            }
+        }
+    }
+}
---
+++ app/code/community/Shopgo/CIS/Model/Currency/Import/Yahoofinance.php
@@ -0,0 +1,36 @@
+<?php
+
+class Shopgo_CIS_Model_Currency_Import_Yahoofinance extends Mage_Directory_Model_Currency_Import_Abstract
+{
+    protected $_url = 'http://finance.yahoo.com/d/quotes.csv?s={{CURRENCY_FROM}}{{CURRENCY_TO}}=X&f=l1&e=.csv';
+    protected $_messages = array();
+
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, $this->_url);
+        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+
+        try {
+            sleep(Mage::getStoreConfig('currency/yahoo_finance/delay')); // Be nice to Yahoo, they don't have a lot of hi-spec servers!
+
+            $handle = fopen($url, "r");
+
+            $exchangeRate = fread($handle, 2000);
+
+            fclose($handle);
+
+            if(!$exchangeRate) {
+                $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s', $url);
+                return null;
+            }
+
+            return (float) $exchangeRate * 1.0; // Change 1.0 to influence rate
+        } catch (Exception $e) {
+            if($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = Mage::helper('directory')->__('Cannot retrieve rate from %s', $url);
+            }
+        }
+    }
+}
